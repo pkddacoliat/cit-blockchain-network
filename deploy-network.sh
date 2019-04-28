9f4035fa7c881d68ce7b9d2368d20e86299bac0d
Patrick Kent Dacoliat <pkddacoliat@gmail.com>
2019-04-28 16:00:33 +0100

    Remove the -rm commands from the deploy-network script

--- deploy-network.sh
+++ deploy-network.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/env bash
 set -e
-rm ./dist/cit-blockchain-network.bna           # remove after development
-rm ./dist/admin@cit-blockchain-network.card    # remove after development
 composer archive create --sourceType dir --sourceName . -a ./dist/cit-blockchain-network.bna
 composer network install -c PeerAdmin@hlfv1 -a ./dist/cit-blockchain-network.bna
 composer network start -c PeerAdmin@hlfv1 -n cit-blockchain-network -V 0.0.1 -A admin -S adminpw -f ./dist/admin@cit-blockchain-network.card
-composer card delete -c admin@cit-blockchain-network    # remove after development
 composer card import -f ./dist/admin@cit-blockchain-network.card
 composer network ping -c admin@cit-blockchain-network  
